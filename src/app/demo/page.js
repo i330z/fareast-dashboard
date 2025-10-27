44117d4c4457e76007088bd1ef018d8d90e65ad0
i330z <izzazahmed134@gmail.com>
2025-10-27 21:35:49 +0530

    demo page reset

--- src/app/demo/page.js
+++ src/app/demo/page.js
@@ -1,20 +1,9 @@
-"use client"
 import React from 'react'
-import TextEditor  from "@/components/CkEditor"
 
-import CustomEditorWrapper from '@/components/CustomEditor';
 function page() {
-    const [editorData, setEditorData] = React.useState("<p>Hello from CKEditor 5!</p>");
-
-  return (
-    <div>
-        <CustomEditorWrapper value="<p>Hello from CKEditor 5!</p>" onDataChange={(data) => setEditorData(data)} />
-        <div className="mt-5 p-4 border rounded">
-            <h2 className="text-lg font-bold mb-2">Editor Data:</h2>
-            <div dangerouslySetInnerHTML={{ __html: editorData }} />
-        </div>  
-    </div>
-  )
+  return (  
+    <div>Demo</div>
+  ) 
 }
 
-export default page
+export default page 
